83da360134ad66e59884f182e93c3bf8d1fd08cd
liveHarshit <live.harshit@hotmail.com>
2018-06-19 18:11:35 +0530

    add networking and show weather data

--- app/src/main/java/com/liveharshit/android/sunshine/MainActivity.java
+++ app/src/main/java/com/liveharshit/android/sunshine/MainActivity.java
@@ -1,11 +1,21 @@
 package com.liveharshit.android.sunshine;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.TelecomManager;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.liveharshit.android.sunshine.data.SunshinePreferences;
+import com.liveharshit.android.sunshine.utilities.NetworkUtils;
+import com.liveharshit.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
 import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,25 +28,52 @@ public class MainActivity extends AppCompatActivity {
 
         mWeatherTextView = (TextView)findViewById(R.id.tv_weather_data);
 
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+        loadWeatherData();
 
-        for (String dummyWeatherDay : dummyWeatherData) {
-            mWeatherTextView.append(dummyWeatherDay + "\n\n\n");
+    }
+
+    private void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String , Void, String []> {
+
+        @Override
+        protected String[] doInBackground(String... strings) {
+            if(strings.length==0) {
+                return null;
+            }
+
+            String location = strings[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                String jsonWeatherResponse = NetworkUtils
+                        .getResponseFromHttpUrl(weatherRequestUrl);
+
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleJsonWeatherData;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if(strings!=null) {
+                for(String weatherData : strings) {
+                    mWeatherTextView.append(weatherData + "\n\n\n");
+                }
+            }
         }
     }
+
+
+
+
 }
